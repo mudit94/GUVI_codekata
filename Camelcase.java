e47c04b896778350827d2e9ae688e4df56f61cac
Mudit Maheshwari <mudit94@gmail.com>
2017-02-11 12:06:37 +0530

    Add files via upload

---
+++ Camelcase.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class Camelcase{
+public static void main(String[] args){
+Scanner sc=new Scanner(System.in);
+String s=sc.nextLine();
+StringBuffer sb=new StringBuffer();
+String a[]=s.split(" ");
+for(int i=0;i<a.length;i++){
+String q=a[i];
+sb.append(Character.toUpperCase(q.charAt(0)));
+if(q.length()>0)
+sb.append(q.substring(1).toLowerCase());
+}
+System.out.println(sb);
+}
+}
