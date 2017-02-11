eedf4a47e267751804d20289c54ecbc36e85b8bd
Mudit Maheshwari <mudit94@gmail.com>
2017-02-11 12:09:34 +0530

    Update Camelcase.java

--- Camelcase.java
+++ Camelcase.java
@@ -3,6 +3,11 @@ public class Camelcase{
 public static void main(String[] args){
 Scanner sc=new Scanner(System.in);
 String s=sc.nextLine();
+  if(s.length()==0)
+  {
+    System.out.println("");
+  }
+  else{
 StringBuffer sb=new StringBuffer();
 String a[]=s.split(" ");
 for(int i=0;i<a.length;i++){
@@ -12,5 +17,6 @@ if(q.length()>0)
 sb.append(q.substring(1).toLowerCase());
 }
 System.out.println(sb);
+}
 }
 }
