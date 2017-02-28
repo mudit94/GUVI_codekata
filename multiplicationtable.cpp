6c226adac0ff730cc38610b12d80fa9361b9364b
Mudit Maheshwari <mudit94@gmail.com>
2017-02-28 10:42:00 +0530

    Update multiplicationtable.cpp

--- multiplicationtable.cpp
+++ multiplicationtable.cpp
@@ -1,10 +1,10 @@
 #include<iostream>
 using namespace std;
 int main(){
-int n;
-cin>>n;
+int n,m;
+cin>>n>>m;
 int i;
-for(i=1;i<=10;i++){
+for(i=1;i<=m;i++){
 cout<<n<<" "<<"*"<<" "<<i<<" "<<"="<<n*i;
 cout<<endl;
 }
